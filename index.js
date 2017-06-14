7da863721600f0e807525bd26b7b555129d04a9f
lixingxing41 <lixingxing41@users.noreply.github.com>
2017-06-14 18:08:17 +0800

    Create index.js

--- index.js
+++ index.js
@@ -83,25 +83,17 @@ function get_time(t) {
 //linebot
 function _bot(){
   bot.on('message', function(event) {
-    if (event.message.type == 'text') {
-      var msg = event.message.text;
-      var replyMsg = '';
-      if (msg.indexOf('濕度') != -1) {
-        replyMsg = '現在濕度為 ' + mHum;
-       // bot.push('U08fdb11d718b720f728c620a3a749139', '現在濕度 ' + mHum);
-      }
-      if (msg.contains('溫度') != -1) {
-        replyMsg = '現在溫度為 ' + mTemp;
-       // bot.push('U08fdb11d718b720f728c620a3a749139', '現在濕度 ' + mTemp);
-      }
-      event.reply(replyMsg).then(function(data) {
-        console.log(replyMsg);
-      }).catch(function(error) {
-        console.log('error');
-      });
-      
-    }
-  });
+  if (event.message.type = 'text') {
+    var msg = event.message.text;
+    event.reply(msg).then(function(data) {
+      // success 
+      console.log(msg);
+    }).catch(function(error) {
+      // error 
+      console.log('error');
+    });
+  }
+});
 //  bot.push('U08fdb11d718b720f728c620a3a749139', '現在濕度 ' + mHum);
 }
 
